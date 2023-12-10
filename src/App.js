cf49afcb39a4be0831ae31f30d2daca77d870fc1
Muhammad Ahsan Ali <91987110+M-Ahsan-Ali-001@users.noreply.github.com>
2023-12-10 18:44:05 +0500

    added code to main!

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Login_Sing from './pages/Login_SignUp';
+
+function App() {
+  return (
+    <div className="App">
+    <Login_Sing/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Login_SignUp.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import "../styles.css"
+function Login_Sing() {
+const [pagePos,setPage] = useState("a")
+
+
+
+  const funcChange=()=>{
+   setPage("b")
+  }
+
+  const funcChange2=()=>{
+    setPage("a")
+   }
+ 
+
+  return (
+    <div>
+    
+       {pagePos === "a"?(
+         <div id="body-login">
+         <div id="card-login">
+           <div id="card-hold">
+
+         
+           <p id="text-log">NoteIT </p>
+           <p id="text-log">Sign-Up</p>
+           <br></br>
+          <form id="form-login">
+          <input id="ipt1" className="input-username" placeholder="Your Email..." type="email" required="true"></input>
+           <br></br>
+           <input id="ipt2" className="input-username" placeholder="Your Password..." type="password" required="true"></input>
+           <br></br>
+           <input id="ipt3" className="input-username" placeholder="Confirm  Password..." type="password" required="true"></input>
+           <br></br>
+
+           <input  id="button-login"  type="submit" value="Signup" ></input>
+           <br></br>
+           <p id="text-navi-login" onClick={funcChange} >Already have an Account ?</p>
+
+
+          </form>
+
+           </div>
+         </div>
+       </div>
+     
+       ): pagePos === "b"?(
+        
+        <div id="body-login">
+          <div id="card-login">
+            <div id="card-hold">
+
+          
+            <p id="text-log">NoteIT </p>
+            <p id="text-log">Sign-In</p>
+            <br></br>
+           <form id="form-login">
+           <input id="ipt1" className="input-username" placeholder="Your Email..." type="email" required="true"></input>
+            <br></br>
+            <input id="ipt2" className="input-username" placeholder="Your Password..." type="password" required="true"></input>
+            <br></br>
+
+            <input  id="button-login"  type="submit" value="Signin" ></input>
+            <br></br>
+            <p id="text-navi-login" onClick={funcChange2} >Create an Account ?</p>
+
+           </form>
+
+            </div>
+          </div>
+        </div>
+       ):null
+
+       }
+    </div>
+  );
+}
+
+export default Login_Sing;
