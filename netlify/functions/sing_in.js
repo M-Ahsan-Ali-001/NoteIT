6ffd89f9e583f9ba22f93fe395d6c7b795c3470a
Muhammad Ahsan Ali <91987110+M-Ahsan-Ali-001@users.noreply.github.com>
2023-12-24 16:23:40 +0500

    sign!

--- netlify/functions/sing_in.js
+++ netlify/functions/sing_in.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const Prometheus = require('promes');
+// const Prometheus = require('promes');
 
-const prometheus = new Prometheus({ appName: 'prometheus' });
+// const prometheus = new Prometheus({ appName: 'prometheus' });
 const { User_signup } = require('../../backend/schema/sign_up_schema.js');
 const mongoURI = "mongodb+srv://Anonymous:FOX.user786@spmnoteapp.ehlsydv.mongodb.net/";
 
@@ -9,11 +9,11 @@ exports.handler = async function (event, context) {
   try {
     await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    prometheus.observe({
-      functionName: context.functionName,
-      event,
-      context,
-    });
+    // prometheus.observe({
+    //   functionName: context.functionName,
+    //   event,
+    //   context,
+    // });
 
     const { email, password } = JSON.parse(event.body);
 
